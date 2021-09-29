1e4b29e166ad6e2d3f1a0d1d81069b529da544c8
Ilian Sapundshiev <ilian@tagspaces.org>
2021-09-29 21:27:25 +0200

    small fixes

--- md-editor/src/App.tsx
+++ md-editor/src/App.tsx
@@ -10,7 +10,7 @@ const App: React.FC = () => {
   const milkdownRef = React.useRef<MilkdownRef>(null);
   const codeMirrorRef = React.useRef<CodeMirrorRef>(null);
   const [mode, setMode] = React.useState('Milkdown');
-  const [ignored, forceUpdate] = useReducer((x) => x + 1, 0);
+  const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
 
   // @ts-ignore
   const isDarkMode = window.theme && window.theme === 'dark';
@@ -20,7 +20,7 @@ const App: React.FC = () => {
   const getContent = () => window.mdContent; //  ? window.mdContent : '');
 
   // @ts-ignore
-  useEventListener('keyup', (event) => {
+  useEventListener('keyup', event => {
     if (
       readOnly() &&
       (event.ctrlKey || event.metaKey) &&
@@ -39,7 +39,7 @@ const App: React.FC = () => {
   });
 
   // @ts-ignore
-  useEventListener('dblclick', (event) => {
+  useEventListener('dblclick', event => {
     if (readOnly()) {
       window.parent.postMessage(
         JSON.stringify({ command: 'editDocument' }),
@@ -219,7 +219,7 @@ const App: React.FC = () => {
     );*/
     window.parent.postMessage(
       JSON.stringify({
-        command: 'contentChangedInEditor',
+        command: 'contentChangedInEditor'
         // filepath: filePath
       }),
       '*'
@@ -239,7 +239,7 @@ const App: React.FC = () => {
   return (
     <div className="container">
       {mode === 'Milkdown' ? (
-        <div style={{ marginTop: 70 }} className="milk">
+        <div className="milk">
           <MilkdownEditor
             ref={milkdownRef}
             content={getContent()}
