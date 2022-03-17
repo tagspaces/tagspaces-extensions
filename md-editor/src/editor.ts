2a309cc7ebef37d869b64d95ced89a8d5c40d38b
sytolk <s.marinov@structmedia.com>
2022-03-17 08:54:56 +0200

    plugin menu

--- md-editor/src/editor.ts
+++ md-editor/src/editor.ts
@@ -18,6 +18,7 @@ import { defaultActions, slash, slashPlugin } from '@milkdown/plugin-slash';
 import { tooltip } from '@milkdown/plugin-tooltip';
 import { nord } from '@milkdown/theme-nord';
 import { AtomList } from '@milkdown/utils';
+import { menu } from '@milkdown/plugin-menu';
 
 const complete =
   (callback: () => void): MilkdownPlugin =>
@@ -28,6 +29,157 @@ const complete =
     callback();
   };
 
+/*export const defaultConfig: Config = [
+  [
+    {
+      type: 'select',
+      text: 'Heading',
+      options: [
+        { id: '1', text: 'Large Heading' },
+        { id: '2', text: 'Medium Heading' },
+        { id: '3', text: 'Small Heading' },
+        { id: '0', text: 'Plain Text' },
+      ],
+      disabled: (view) => {
+        const { state } = view;
+        const setToHeading = (level: number) => setBlockType(state.schema.nodes.heading, { level })(state);
+        return (
+          !(view.state.selection instanceof TextSelection) ||
+          !(setToHeading(1) || setToHeading(2) || setToHeading(3))
+        );
+      },
+      onSelect: (id) => (id ? ['TurnIntoHeading', Number(id)] : ['TurnIntoText', null]),
+    },
+  ],
+  [
+    {
+      type: 'button',
+      icon: 'undo',
+      key: 'Undo',
+      disabled: (view) => {
+        return !undo(view.state);
+      },
+    },
+    {
+      type: 'button',
+      icon: 'redo',
+      key: 'Redo',
+      disabled: (view) => {
+        return !redo(view.state);
+      },
+    },
+  ],
+  [
+    {
+      type: 'button',
+      icon: 'bold',
+      key: 'ToggleBold',
+      active: (view) => hasMark(view.state, view.state.schema.marks.strong),
+      disabled: (view) => !view.state.schema.marks.strong,
+    },
+    {
+      type: 'button',
+      icon: 'italic',
+      key: 'ToggleItalic',
+      active: (view) => hasMark(view.state, view.state.schema.marks.em),
+      disabled: (view) => !view.state.schema.marks.em,
+    },
+    {
+      type: 'button',
+      icon: 'strikeThrough',
+      key: 'ToggleStrikeThrough',
+      active: (view) => hasMark(view.state, view.state.schema.marks.strike_through),
+      disabled: (view) => !view.state.schema.marks.strike_through,
+    },
+  ],
+  [
+    {
+      type: 'button',
+      icon: 'bulletList',
+      key: 'WrapInBulletList',
+      disabled: (view) => {
+        const { state } = view;
+        return !wrapIn(state.schema.nodes.bullet_list)(state);
+      },
+    },
+    {
+      type: 'button',
+      icon: 'orderedList',
+      key: 'WrapInOrderedList',
+      disabled: (view) => {
+        const { state } = view;
+        return !wrapIn(state.schema.nodes.ordered_list)(state);
+      },
+    },
+    {
+      type: 'button',
+      icon: 'taskList',
+      key: 'TurnIntoTaskList',
+      disabled: (view) => {
+        const { state } = view;
+        return !wrapIn(state.schema.nodes.task_list_item)(state);
+      },
+    },
+    {
+      type: 'button',
+      icon: 'liftList',
+      key: 'LiftListItem',
+      disabled: (view) => {
+        const { state } = view;
+        return !liftListItem(state.schema.nodes.list_item)(state);
+      },
+    },
+    {
+      type: 'button',
+      icon: 'sinkList',
+      key: 'SinkListItem',
+      disabled: (view) => {
+        const { state } = view;
+        return !sinkListItem(state.schema.nodes.list_item)(state);
+      },
+    },
+  ],
+  [
+    {
+      type: 'button',
+      icon: 'link',
+      key: 'ToggleLink',
+      active: (view) => hasMark(view.state, view.state.schema.marks.link),
+    },
+    {
+      type: 'button',
+      icon: 'image',
+      key: 'InsertImage',
+    },
+    {
+      type: 'button',
+      icon: 'table',
+      key: 'InsertTable',
+    },
+    {
+      type: 'button',
+      icon: 'code',
+      key: 'TurnIntoCodeFence',
+    },
+  ],
+  [
+    {
+      type: 'button',
+      icon: 'quote',
+      key: 'WrapInBlockquote',
+    },
+    {
+      type: 'button',
+      icon: 'divider',
+      key: 'InsertHr',
+    },
+    {
+      type: 'button',
+      icon: 'select',
+      key: SelectParent,
+    },
+  ],
+];*/
 export const createEditor = (
   root: HTMLElement | null,
   defaultValue: string,
@@ -52,6 +204,7 @@ export const createEditor = (
     // .use(commonmark)
     .use(nodes)
     // .use(gfm)
+    .use(menu())
     .use(complete(() => setEditorReady(true)))
     .use(clipboard)
     .use(listener)
