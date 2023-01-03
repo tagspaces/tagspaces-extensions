e404c1c1c59f69424894ff4358be8bc6f5181579
Ilian Sapundshiev <ilian@tagspaces.org>
2023-01-03 10:13:56 +0100

    removing unneeded libraries adding latest versions

--- common/update-libs.js
+++ common/update-libs.js
@@ -10,26 +10,6 @@ sh.mkdir('libs/jquery');
 sh.cp('-R', 'node_modules/jquery/dist', 'libs/jquery');
 sh.cp('node_modules/jquery/LICENSE.txt', 'libs/jquery');
 
-sh.rm('-rf', 'libs/jquery-i18next/');
-sh.mkdir('libs/jquery-i18next');
-sh.cp('node_modules/jquery-i18next/LICENSE', 'libs/jquery-i18next');
-sh.cp('node_modules/jquery-i18next/jquery-i18next.js', 'libs/jquery-i18next');
-sh.cp(
-  'node_modules/jquery-i18next/jquery-i18next.min.js',
-  'libs/jquery-i18next'
-);
-
-// sh.rm('-rf', 'libs/bootstrap/');
-// sh.mkdir('libs/bootstrap');
-// sh.cp('-R', 'node_modules/bootstrap/dist', 'libs/bootstrap');
-// sh.cp('node_modules/bootstrap/LICENSE', 'libs/bootstrap');
-
-// sh.rm('-rf', 'libs/bootswatch/');
-// sh.mkdir('libs/bootswatch');
-// sh.cp('-R', 'node_modules/bootswatch/paper', 'libs/bootswatch');
-// sh.cp('-R', 'node_modules/bootswatch/fonts', 'libs/bootswatch');
-// sh.cp('node_modules/bootswatch/LICENSE', 'libs/bootswatch');
-
 sh.rm('-rf', 'libs/bootstrap5/');
 sh.mkdir('libs/bootstrap5');
 sh.cp('node_modules/bootstrap5/LICENSE', 'libs/bootstrap5');
@@ -55,12 +35,6 @@ sh.cp('node_modules/viewerjs/dist/viewer.min.js', 'libs/viewerjs');
 sh.cp('node_modules/viewerjs/dist/viewer.min.css', 'libs/viewerjs');
 sh.cp('node_modules/viewerjs/LICENSE', 'libs/viewerjs');
 
-sh.rm('-rf', 'libs/font-awesome/');
-sh.mkdir('libs/font-awesome');
-sh.cp('-R', 'node_modules/font-awesome/css', 'libs/font-awesome');
-sh.cp('-R', 'node_modules/font-awesome/fonts', 'libs/font-awesome');
-sh.cp('node_modules/bootswatch/README.md', 'libs/font-awesome');
-
 sh.rm('-rf', 'libs/marked/');
 sh.mkdir('libs/marked');
 sh.cp('node_modules/marked/LICENSE.md', 'libs/marked');
@@ -85,21 +59,14 @@ sh.rm('-rf', 'libs/i18next/');
 sh.mkdir('libs/i18next');
 sh.cp('node_modules/i18next/LICENSE', 'libs/i18next');
 sh.cp('node_modules/i18next/i18next.min.js', 'libs/i18next');
-sh.cp('node_modules/i18next/i18next.js', 'libs/i18next');
-sh.cp('node_modules/i18next/karma.backward.conf.js', 'libs/i18next');
-
-sh.rm('-rf', 'libs/mousetrap/');
-sh.mkdir('libs/mousetrap');
-sh.cp('node_modules/mousetrap/README.md', 'libs/mousetrap');
-sh.cp('node_modules/mousetrap/mousetrap.min.js', 'libs/mousetrap');
-sh.cp('node_modules/mousetrap/mousetrap.js', 'libs/mousetrap');
-sh.cp('-R', 'node_modules/mousetrap/plugins', 'libs/mousetrap');
+// sh.cp('node_modules/i18next/i18next.js', 'libs/i18next');
+// sh.cp('node_modules/i18next/karma.backward.conf.js', 'libs/i18next');
 
 sh.rm('-rf', 'libs/readability/');
 sh.mkdir('libs/readability');
-sh.cp('node_modules/readability/README.md', 'libs/readability');
-sh.cp('node_modules/readability/JSDOMParser.js', 'libs/readability');
-sh.cp('node_modules/readability/Readability.js', 'libs/readability');
+sh.cp('node_modules/@mozilla/readability/README.md', 'libs/readability');
+sh.cp('node_modules/@mozilla/readability/JSDOMParser.js', 'libs/readability');
+sh.cp('node_modules/@mozilla/readability/Readability.js', 'libs/readability');
 
 // sh.rm('-rf', 'html-editor/libs/summernote');
 // sh.mkdir('html-editor/libs/summernote');
