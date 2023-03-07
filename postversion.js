25cda786028b06a1c9ac1bd678d0f5a1ef6b5ce2
sytolk <s.marinov@structmedia.com>
2023-03-07 10:28:20 +0200

    version bump

---
+++ postversion.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname), { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.error('Error reading directory:', err);
+    reject(err);
+    return;
+  }
+
+  const packageJson = fs.readFileSync(path.join(__dirname, 'package.json'));
+  const rootPackageJson = JSON.parse(packageJson);
+
+  // Filter subdirectories
+  const subDirectories = files.filter(
+    (file) =>
+      file.isDirectory() &&
+      !file.name.startsWith('.') &&
+      file.name !== 'node_modules'
+  );
+
+  subDirectories.map((dir) => {
+    const pluginJsonPath = path.join(__dirname, dir.name, 'package.json');
+    try {
+      const packageJsonContent = fs.readFileSync(pluginJsonPath);
+      const packageJsonObj = JSON.parse(packageJsonContent);
+      if (packageJsonObj.version !== rootPackageJson.version) {
+        packageJsonObj.version = rootPackageJson.version;
+        fs.writeFile(
+          pluginJsonPath,
+          JSON.stringify(packageJsonObj, null, 2),
+          'utf8',
+          () => {
+            console.log(
+              'Successfully update version:' + packageJsonObj.version
+            );
+          }
+        );
+      }
+    } catch (ex) {
+      console.debug(
+        'Update package.json version: ' + dir.name + ' error:' + ex.message
+      );
+    }
+  });
+});
