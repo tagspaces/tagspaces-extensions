e8748c326f6633837a47a5d3be1a5b33aea1adaf
Ilian Sapundshiev <ilian@tagspaces.org>
2024-04-02 13:26:42 +0200

    updating dompurify to 3.0.11

--- libs/dompurify/dist/purify.es.mjs
+++ libs/dompurify/dist/purify.es.mjs
@@ -1,7 +1,5 @@
 /*! @license DOMPurify 3.0.11 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/3.0.11/LICENSE */
 
-'use strict';
-
 const {
   entries,
   setPrototypeOf,
@@ -1511,5 +1509,5 @@ function createDOMPurify() {
 }
 var purify = createDOMPurify();
 
-module.exports = purify;
-//# sourceMappingURL=purify.cjs.js.map
+export { purify as default };
+//# sourceMappingURL=purify.es.mjs.map
