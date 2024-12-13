b1dfd6689ecd3cfcbdc55443c9632e7d38793aac
sytolk <s.marinov@structmedia.com>
2024-12-13 14:59:53 +0200

    remove old text editor

--- text-editor-old/scripts/update-libs.js
+++
@@ -1,9 +0,0 @@
-const sh = require('shelljs');
-
-sh.rm('-rf', 'libs/codemirror/');
-sh.mkdir('libs/codemirror');
-sh.cp('node_modules/codemirror/LICENSE', 'libs/codemirror');
-sh.cp('-R', 'node_modules/codemirror/addon/', 'libs/codemirror');
-sh.cp('-R', 'node_modules/codemirror/lib/', 'libs/codemirror');
-sh.cp('-R', 'node_modules/codemirror/mode/', 'libs/codemirror');
-sh.cp('-R', 'node_modules/codemirror/theme/', 'libs/codemirror');
