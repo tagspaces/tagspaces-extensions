50986a94d89017cc5d5c85eb70bfef58ff1453fc
sytolk <s.marinov@structmedia.com>
2025-03-11 15:00:31 +0200

    extensions v163

--- md-editor-crepe/src/main.tsx
+++ md-editor-crepe/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { MilkdownEditor } from './Editor';
 import { MilkdownProvider } from '@milkdown/react';
 import { getParameterByName } from './utils';
+import { ProsemirrorAdapterProvider } from '@prosemirror-adapter/react';
 
 const root$ = document.getElementById('app');
 if (!root$) throw new Error('No root element found');
@@ -15,11 +16,13 @@ const theme = getParameterByName('theme');
 root.render(
   <StrictMode>
     <MilkdownProvider>
-      <MilkdownEditor
-        isEditMode={!!isEditMode}
-        readOnly={!!readOnly}
-        theme={theme}
-      />
+      <ProsemirrorAdapterProvider>
+        <MilkdownEditor
+          isEditMode={!!isEditMode}
+          readOnly={!!readOnly}
+          theme={theme}
+        />
+      </ProsemirrorAdapterProvider>
     </MilkdownProvider>
   </StrictMode>,
 );
